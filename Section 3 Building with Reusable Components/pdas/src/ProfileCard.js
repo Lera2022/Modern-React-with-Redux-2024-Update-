ecf8f9fffd2269170f5a4c50839083ae7963c44b
Lera <valeriiakolesnikova21@gmail.com>
2024-06-04 17:58:14 +0500

    3 section modified

--- Section 3 Building with Reusable Components/pdas/src/ProfileCard.js
+++ Section 3 Building with Reusable Components/pdas/src/ProfileCard.js
@@ -1,5 +1,17 @@
-function ProfileCard() {
-  return <div>Profile Card!!!</div>;
-}
+// function ProfileCard(props) {
+  function ProfileCard({title, handle}) {
+  // const title = props.title;
+  // const handle = props.handle;
 
+  // const {title, handle} = props;
+  
+  return (
+    <div>
+      {/* <div>Title is {props.title}</div>
+      <div>Handle is {props.handle}</div>       */}
+      <div>Title is {title}</div>
+      <div>Handle is {handle}</div>
+    </div>
+  );
+}
 export default ProfileCard;
