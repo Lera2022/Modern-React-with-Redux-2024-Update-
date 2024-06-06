c48c572ffdf8898943729d6256c4e3f57a614cdd
Lera <valeriiakolesnikova21@gmail.com>
2024-06-06 17:46:19 +0500

    3 section modified

--- Section 3 Building with Reusable Components/pdas/src/App.js
+++ Section 3 Building with Reusable Components/pdas/src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css'
 import ProfileCard from './ProfileCard';
 import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
@@ -14,10 +15,24 @@ function App() {
       {/* <img src={AlexaImage} />
       <img src={SiriImage} />
       <img src='https://placebeard.it/420/320' /> */}
-
-      <ProfileCard title = "Alexa" handle = "@alexa99" image={AlexaImage}/>
-      <ProfileCard title = "Cortana" handle = "@cortana32" image={CortanaImage}/>
-      <ProfileCard title = "Siri" handle = "@siri01" image={SiriImage}/>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            {/* <div className="column is-4"> */}
+            <div className="column is-3">
+              <ProfileCard title = "Alexa" handle = "@alexa99" image={AlexaImage}/>
+            </div>            
+            {/* <div className="column is-4"> */}
+            <div className="column is-3">
+              <ProfileCard title = "Cortana" handle = "@cortana32" image={CortanaImage}/>
+            </div>            
+            {/* <div className="column is-4"> */}
+            <div className="column is-3">
+              <ProfileCard title = "Siri" handle = "@siri01" image={SiriImage}/>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- Section 3 Building with Reusable Components/pdas/src/ProfileCard.js
+++ Section 3 Building with Reusable Components/pdas/src/ProfileCard.js
@@ -6,12 +6,23 @@
   console.log(title, handle);
   
   return (
-    <div>
+    <div className="card">
       {/* <div>Title is {props.title}</div>
       <div>Handle is {props.handle}</div>       */}
-      <img src={image} alt="pda logo" />
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="pda logo" />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+        {/* <p className="title is-4">Title is {title}</p>
+        <p className="subtitle is-6">Handle is {handle}</p>           */}
+        <p className="title is-4">{title}</p>
+        <p className="subtitle is-6">{handle}</p>
+        </div>
+      </div>
     </div>
   );
 }
