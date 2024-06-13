312f7f0c4664a46d5a59255a30f4efd806bf77c2
Lera <valeriiakolesnikova21@gmail.com>
2024-06-13 19:43:22 +0500

    4 section modified

--- Section 4 State. How to Change Your App/animals/src/App.js
+++ Section 4 State. How to Change Your App/animals/src/App.js
@@ -1,12 +1,23 @@
+import { useState } from 'react';
+
 function App() {
+    const [count, setCount] = useState(0);
+
+    // NEVER GOING TO...    count = 123;    setCount(123);
+
     const handleClick = () => {
-         console.log('Button was clicked!');
+        //  console.log('Button was clicked!');
+        setCount(count + 1);
     };
 
     return (
         <div>
             {/* Show animal list here! */}
             <button onClick={handleClick}>Add Animal</button>
+            <div>Number of animals: {count}</div>
+            {/* <button onClick={() => console.log('Button was clicked!')}>
+                Add Animal
+            </button> */}
         </div>
     );
 }
