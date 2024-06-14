c26a3477b78a9da232b574e804e4c2a70ff283ad
Lera <valeriiakolesnikova21@gmail.com>
2024-06-14 17:19:17 +0500

    4 section finished

--- Section 4 State. How to Change Your App/animals/src/App.js
+++ Section 4 State. How to Change Your App/animals/src/App.js
@@ -1,20 +1,53 @@
+import './App.css';
 import { useState } from 'react';
+import AnimalShow from './AnimalShow';
+
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)]
+}
+
+// console.log(getRandomAnimal());
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [animals, setAnimals] = useState([]);
+
+    // function makeArray() {
+    //     return [1, 10, 32, 40];
+    // }
+
+    // // const myArray = makeArray();
+    // // const firstElement = myArray[0];
+    // // const secondElement = myArray[1];
+
+    // const [firstElement, secondElement] = makeArray();
+    // console.log(firstElement, secondElement);
+
+    // const [count, setCount] = useState(0);
+    // const [value, setValue] = useState(0);
+
+    // console.log(useState(50));
 
     // NEVER GOING TO...    count = 123;    setCount(123);
 
     const handleClick = () => {
+        setAnimals([...animals, getRandomAnimal()])
         //  console.log('Button was clicked!');
-        setCount(count + 1);
+        // setCount(count + 1);
     };
 
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />
+    })
+
     return (
-        <div>
+        <div className='app'>
             {/* Show animal list here! */}
             <button onClick={handleClick}>Add Animal</button>
-            <div>Number of animals: {count}</div>
+            {/* <div>{animals}</div> */}
+            <div className='animal-list'>{renderedAnimals}</div>
+            {/* <div>Number of animals: {count}</div> */}
             {/* <button onClick={() => console.log('Button was clicked!')}>
                 Add Animal
             </button> */}
