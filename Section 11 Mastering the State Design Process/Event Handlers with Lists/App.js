4684599c0d4576d2d243ae028a0cbe0b517b8350
Lera <valeriiakolesnikova21@gmail.com>
2024-06-26 13:59:29 +0500

    11 section finished

---
+++ Section 11 Mastering the State Design Process/Event Handlers with Lists/App.js
@@ -0,0 +1,16 @@
+// You shouldn't need to change anything in this file
+
+import React from 'react';
+import { ImageList } from './ImageList';
+
+const images = [
+  {id: 'a', name: 'Leaf', src: 'https://picsum.photos/seed/abd/200/300' },
+  {id: 'b', name: 'House', src: 'https://picsum.photos/seed/gqk/200/300' },
+  {id: 'c', name: 'Tree', src: 'https://picsum.photos/seed/ias/200/300' },
+];
+
+function App() {
+    return <ImageList images={images} />
+}
+
+export default App;
--- Section 11 Mastering the State Design Process/comps/src/components/Accordion.js
+++ Section 11 Mastering the State Design Process/comps/src/components/Accordion.js
@@ -1,32 +1,69 @@
 import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+
 
 function Accordion({ items }) {
-    const [expandedIndex, setExpandedIndex] = useState(0);
+    const [expandedIndex, setExpandedIndex] = useState(-1);
 
+    const handleClick = (nextIndex) => {
+        console.log('STALE version of expandedIndex', expandedIndex);   // $0.click(); $0.click();
+
+        setExpandedIndex((currentExpandedIndex) => {
+            console.log('UP TO DATE version', currentExpandedIndex);
+            if (currentExpandedIndex === nextIndex) {
+                return -1;
+            } else {
+                return nextIndex;
+            }
+        });
+
+        // console.log(expandedIndex);
+    //     if (expandedIndex === nextIndex) {
+    //         setExpandedIndex(-1);
+    //     } else {
+    //         setExpandedIndex(nextIndex);
+    //     }
+    };
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
 
         // console.log(isExpanded);
-
         // const content = isExpanded && <div>{item.content}</div>
-
         // if (index === expandedIndex) {
         //     console.log('expanded');
         // } else {
         //     console.log('collapsed');
         // }
 
+        const icon = <span className='text-2xl'>
+            {/* {isExpanded ? 'DOWN' : 'LEFT'} */}
+            {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+        </span>
+
         return (
             <div key={item.id}>
-                <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+                {/* <div onClick={handleClick}>{item.label}</div> */}
+                <div className='flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer' onClick={() => handleClick(index)}>
+                    {item.label}
+                    {icon}
+                </div>
+
+                {/* <div onClick={() => setExpandedIndex(index)}>{item.label}</div> */}
+                {/* <div onClick={() => {
+                    console.log(index);
+                    setExpandedIndex(index)
+                    }}
+                    >
+                    {item.label}
+                </div> */}
                 {/* <div>{item.content}</div> */}
                 {/* {content} */}
-                {isExpanded && <div>{item.content}</div>}
+                {isExpanded && <div className='border-b p-5 '>{item.content}</div>}
             </div>
         );
     });
 
-    return <div>{renderedItems}</div>;
+    return <div className='border-x border-t rounded'>{renderedItems}</div>;
 }
 
 export default Accordion;
