1df906d849cdad2ab74227a2ca7b95a4e77bae0e
Lera <valeriiakolesnikova21@gmail.com>
2024-07-12 18:19:05 +0500

    feat: add section 19

--- Section 18 Into the World of Reducers/src/pages/CounterPage.js
+++ Section 18 Into the World of Reducers/src/pages/CounterPage.js
@@ -1,5 +1,5 @@
 import { produce } from "immer";
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
