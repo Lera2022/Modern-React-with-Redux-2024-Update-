e3e0476a8e0249219196b0b0430fe1322b1532c3
Lera <valeriiakolesnikova21@gmail.com>
2024-07-15 17:03:25 +0500

    feat: modify section 19

--- Section 19 Dive Into Redux Toolkit/src/store/index.js
+++ Section 19 Dive Into Redux Toolkit/src/store/index.js
@@ -4,9 +4,15 @@ const songsSlice = createSlice({
   name: "song",
   initialState: [],
   reducers: {
+    // 'song' + '/' + 'addSong' = 'song/addSong'
     addSong(state, action) {
+      // STATE IS NOT THE BIG STATE OBJECT
+      // IN THE STORE
+      // IT IS THE PIECE OF STATE MANAGED
+      // BY THIS REDUCER
       state.push(action.payload);
     },
+    // 'song' + '/' + 'removeSong' = 'song/removeSong'
     removeSong(state, action) {
       //
     },
@@ -16,7 +22,33 @@ const songsSlice = createSlice({
 const store = configureStore({
   reducer: {
     songs: songsSlice.reducer,
+    // asdfghjkl: songsSlice.reducer,
   },
 });
 
-console.log(object);
+// console.log(songsSlice.actions)
+// console.log(songsSlice.actions.addSong())
+// console.log(songsSlice.actions.addSong("Some song!"))
+
+// console.log(store);
+
+// const startingState = store.getState();
+// // console.log(startingState);
+// console.log(JSON.stringify(startingState));
+
+// store.dispatch(
+//   //   {
+//   //   type: "song/addSong",
+//   //   payload: "New Song!!!",
+//   // }
+//   songsSlice.actions.addSong("Some song!")
+// );
+
+// const finalState = store.getState();
+// console.log(JSON.stringify(finalState));
+
+export { store };
+
+// console.log(songsSlice.actions.addSong())
+
+export const { addSong } = songsSlice.actions;
